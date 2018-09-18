f2d7a86087d7170e5512c541627ee34afa20a6b6
yaoleihxr <yaoleihxr@163.com>
2018-09-18 16:29:20 +0800

    YL

--- tensorflow_learn.py
+++ tensorflow_learn.py
@@ -3,3 +3,18 @@
 
 
 import tensorflow as tf
+
+def test_tensorboard():
+    a = tf.constant(2)
+    b = tf.constant(3)
+    x = tf.add(a, b)
+    with tf.Session() as sess:
+        writer = tf.summary.FileWriter('E:/graph', sess.graph)
+        print(sess.run(x))
+        writer.close()
+        # cmd中输入tensorboard - -logdir = "E:/graph"可打开tensorboard
+        # 需要将cmd切换到相应目录，否则会报错寻找不到文件
+
+
+if __name__ == '__main__':
+    test_tensorboard()
