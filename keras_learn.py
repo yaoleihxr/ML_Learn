a5145e3d3d2e98301093040cc33462bdc69101b8
yaoleihxr <yaoleihxr@163.com>
2018-10-10 17:44:45 +0800

    YL

--- keras_learn.py
+++ keras_learn.py
@@ -3,3 +3,46 @@
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.utils.np_utils import to_categorical
+from keras.datasets import mnist
+from keras.utils import plot_model
+
+# load_Data #
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# (batch, height, width, channels)
+x_train = x_train.reshape(-1,28,28,1)
+x_test = x_test.reshape(-1,28,28,1)
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+# print(x_train, y_train)
+# print(x_train.shape, y_train.shape)
+# load_Data #
+
+
+# model #
+model = Sequential()
+# layer 2
+# 作为模型第一层时，需要提供 input_shape 参数
+model.add(Conv2D(filters=6, kernel_size=(3,3), strides=(1,1), input_shape=x_train.shape[1:],
+                 data_format='channel_last', padding='valid', activation='relu',
+                 kernel_initializer='uniform'))
+# layer 3
+model.add(MaxPooling2D(2,2))
+# layer 4
+model.add(Conv2D(filters=16, kernel_size=(3,3), strides=(1,1), data_format='channel_last',
+                 padding='valid', activation='relu', kernel_initializer='uniform'))
+# layer 5
+model.add(MaxPooling2D(2,2))
+# layer 6
+model.add(Conv2D(filters=120, kernel_size=(5,5), strides=(1,1), data_format='channel_last',
+                 padding='valid', activation='relu', kernel_initializer='uniform'))
+model.add(Flatten())
+# layer 7
+model.add(Dense(84, activation='relu'))
+# layer 8
+model.add(Dense(10, activation='softmax'))
+# print
+model.summary()
+# model #
+
