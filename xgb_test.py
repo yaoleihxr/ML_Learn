eeffda267cd35556db5977cfa0745f50cac4b2ea
yaoleihxr <yaoleihxr@163.com>
2018-11-14 15:00:37 +0800

    YL

--- xgb_test.py
+++ xgb_test.py
@@ -4,13 +4,15 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+from xgboost import plot_importance
+import matplotlib.pyplot as plt
 from sklearn import metrics
 from xgboost.sklearn import XGBClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 
-# path = '/sas/cxdx/yl/test/'
-path = 'E:/'
+path = '/sas/cxdx/yl/test/'
+# path = 'E:/'
 
 def load_data():
     train = pd.read_csv(path + 'MNIST.train.csv')
@@ -28,7 +30,6 @@ def train_split(train):
 def model_fit(xgb_model, dtrain, cv_folds=5, early_stopping_rounds=50):
     xgb_train, xgb_val, train_label, val_label = train_split(train)
     xgb_param = xgb_model.get_xgb_params()
-    # print(xgb_param) # dict
     xgb_train = xgb.DMatrix(xgb_train, label=train_label)
     xgb_test = xgb.DMatrix(xgb_val)
     cv_result = xgb.cv(xgb_param, xgb_train, nfold=5, num_boost_round=300,
@@ -38,12 +39,17 @@ def model_fit(xgb_model, dtrain, cv_folds=5, early_stopping_rounds=50):
 
     xgb_model.set_params(n_estimators=cv_result.shape[0])
     # eval_metric[default according to objective] eval_metric='mlogloss'
+    # xgb_model.set_params(n_estimators=50)
     xgb_model.fit(dtrain.values[:, 1:], dtrain.values[:, 0])
+    print('model finish')
+
     dtrain_pred = xgb_model.predict(dtrain.values[:, 1:])
     dtrain_pred_prob = xgb_model.predict_proba(dtrain.values[:, 1:])
-    feat = xgb_model.get_booster().get_fscore()
-    print(type(feat))
+
+    xgb_model.get_booster().save_model(path + 'xgb.model')
+    print(xgb_model.feature_importances_)
     print('准确率 : %.4g' % metrics.accuracy_score(dtrain.values[:, 0], dtrain_pred))
+
 
 def grid_search(xgb_model, param_search, param_set, dtrain):
     xgb_gs = XGBClassifier(booster='gbtree', learning_rate=0.1, n_estimators=300, max_depth=6,
@@ -59,6 +65,18 @@ def grid_search(xgb_model, param_search, param_set, dtrain):
     print(gsearch.best_params_)
     return gsearch
 
+def load_model(model_path):
+    model = xgb.Booster()
+    model.load_model(model_path)
+    return model
+
+def plot_feat_imp(xgb_model, feat_num):
+    # fig = plt.figure(figsize=(15,15))
+    # ax = fig.add_subplot(111)
+    fig, ax = plt.subplots(figsize=(15,15))
+    plot_importance(xgb_model, ax=ax, max_num_features=feat_num)
+    plt.show()
+
 
 if __name__ == '__main__':
     train, test = load_data()
@@ -71,18 +89,18 @@ if __name__ == '__main__':
     # xgb_model.get_booster().save_model('E:/xgb.model')
     # clf = xgb.Booster(model_file='E:/xgb.model')
 
-    param_test1 = {'max_depth':range(3,10,2), 'min_child_weight':range(1,6,2)}
-    param_set1 = {'n_estimator':100}
-    gsearch1 = grid_search(xgb_model, param_test1, param_set1, train)
-
-    param_test2 = {'gamma': [i/10.0 for i in range(0,6)]}
-    param_set2 = {'n_estimator':100, 'max_depth':6, 'min_child_weight':1}
-    gsearch2 = grid_search(xgb_model, param_test2, param_set2, train)
-
-    param_test3 = {'subsample': [i/10.0 for i in range(6,10)],
-                   'colsample_bytree': [i/10.0 for i in range(6,10)]}
-    param_set3 = {'n_estimator': 100, 'max_depth': 6, 'min_child_weight': 1, 'gamma': 0}
-    gsearch3 = grid_search(xgb_model, param_test3, param_set3, train)
+    # param_test1 = {'max_depth':range(3,10,2), 'min_child_weight':range(1,6,2)}
+    # param_set1 = {'n_estimators':100}
+    # gsearch1 = grid_search(xgb_model, param_test1, param_set1, train)
+    #
+    # param_test2 = {'gamma': [i/10.0 for i in range(0,6)]}
+    # param_set2 = {'n_estimators':100, 'max_depth':6, 'min_child_weight':1}
+    # gsearch2 = grid_search(xgb_model, param_test2, param_set2, train)
+    #
+    # param_test3 = {'subsample': [i/10.0 for i in range(6,10)],
+    #                'colsample_bytree': [i/10.0 for i in range(6,10)]}
+    # param_set3 = {'n_estimators': 100, 'max_depth': 6, 'min_child_weight': 1, 'gamma': 0}
+    # gsearch3 = grid_search(xgb_model, param_test3, param_set3, train)
 
 
 
