d2bae8dae5a2e86a1f1880142239f1ba227e2506
yaoleihxr <yaoleihxr@163.com>
2018-11-19 15:11:39 +0800

    YL

--- rf_test.py
+++ rf_test.py
@@ -1,3 +1,12 @@
 # /usr/bin/python
 # -*- encoding:utf-8 -*-
 
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+import test_data
+
+
